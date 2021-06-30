3e0dc83b50128430556cb290a336ec34887a4372
Gideon Idoko <iamgideonidoko@gmail.com>
2021-06-30 16:31:34 +0100

    do `/adm/manage-file-upload` interface

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import { Provider } from 'react-redux';
 import store from './reduxstore/store';
 import SiteRouter from './components/SiteRouter';
+import 'neuicons';
 
 function App() {
     return (
--- src/pages/admin/ManageFileUpload.js
+++ src/pages/admin/ManageFileUpload.js
@@ -1,6 +1,7 @@
 import {connect} from 'react-redux';
 import {logout} from './../../reduxstore/actions/authActions';
 import WithAdminAuth from '../../layouts/WithAdminAuth';
+import '../../styles/AdminPortalGlobal.css';
 
 const ManageFileUpload = (props) => {
     console.log("From manage file upload");
@@ -9,8 +10,42 @@ const ManageFileUpload = (props) => {
 
     return (
         <WithAdminAuth>
-            {props.isAuthenticated && <div>
-                <h2>Manage File Upload</h2>
+            {props.isAuthenticated && <div className="ap-main-section">
+                <div className="ap-main-section-header ap-box">
+                    <h2>Manage File Upload</h2>
+                </div>
+
+                <div className="ap-box">
+                    <h3>Upload Photos</h3>
+                    <p>Click on the upload button below to upload a new photo.</p>
+                    <div className="file-input-container">
+                        <label htmlFor="photo-upload-input" className="photo-upload-select"><i className="neu-attachment"></i> Select Photo</label>
+                        <input type="file" name="photo-upload-input" id="photo-upload-input" />
+                        <button className="document-upload-btn"><i className="neu-upload"></i> Upload Photo</button>
+                    </div>
+
+                </div>
+
+                <div className="ap-box">
+                    <h3>Upload Documents</h3>
+                    <p>Click the upload button below to upload a new document.</p>
+                    <div className="file-input-container">
+                        <label htmlFor="document-upload-input" className="document-upload-select"><span><i className="neu-attachment"></i> Select Document</span></label>
+                        <input type="file" name="document-upload-input" id="document-upload-input" />
+                        <button className="document-upload-btn"><i className="neu-upload"></i> Upload Document</button>
+                    </div>
+                </div>
+
+                <div className="ap-box">
+                    <h3>View Photos</h3>
+                </div>
+
+                <div className="ap-box">
+                    <h3>Delete Photos</h3>
+                </div>
+                <div className="ap-box">
+                    <h3>Delete Documents</h3>
+                </div>
                 
             </div>}
         </WithAdminAuth>
