5837524e352103759966575f36a25dc0eaa06e02
Gideon Idoko <iamgideonidoko@gmail.com>
2021-07-07 15:30:35 +0100

    register `pageReducer`

--- src/reduxstore/reducers/index.js
+++ src/reduxstore/reducers/index.js
@@ -3,10 +3,12 @@ import authReducer from './authReducer';
 import errorReducer from './errorReducer';
 import firebaseReducer from './firebaseReducer';
 import assetReducer from './assetReducer';
+import pageReducer from './pageReducer';
 
 export default combineReducers({
     auth: authReducer,
     error: errorReducer,
     fire: firebaseReducer,
-    asset: assetReducer
+    asset: assetReducer,
+    page: pageReducer
 });
