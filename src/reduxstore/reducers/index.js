e6b764f8ca7f7cd6df3a766c1e05b25222e30f54
Gideon Idoko <iamgideonidoko@gmail.com>
2021-07-13 00:56:37 +0100

    register more reducers

--- src/reduxstore/reducers/index.js
+++ src/reduxstore/reducers/index.js
@@ -4,11 +4,19 @@ import errorReducer from './errorReducer';
 import firebaseReducer from './firebaseReducer';
 import assetReducer from './assetReducer';
 import pageReducer from './pageReducer';
+import accessReducer from './accessReducer';
+import galleryReducer from './galleryReducer';
+import faqReducer from './faqReducer';
+import mgmtProfileReducer from './mgmtProfileReducer';
 
 export default combineReducers({
     auth: authReducer,
     error: errorReducer,
     fire: firebaseReducer,
     asset: assetReducer,
-    page: pageReducer
+    page: pageReducer,
+    access: accessReducer,
+    gallery: galleryReducer,
+    faq: faqReducer,
+    mgmtProfile: mgmtProfileReducer
 });
