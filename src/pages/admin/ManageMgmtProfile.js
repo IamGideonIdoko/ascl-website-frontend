fe756115911f519b2b068a4e6a5df6d94a7aa6aa
Gideon Idoko <iamgideonidoko@gmail.com>
2021-08-11 08:39:54 +0100

    create new profile form

--- src/pages/admin/ManageMgmtProfile.js
+++ src/pages/admin/ManageMgmtProfile.js
@@ -1,9 +1,17 @@
-// import {useState} from 'react';
+import {useState} from 'react';
 import {connect} from 'react-redux';
 import WithAdminAuth from '../../layouts/WithAdminAuth';
 import AssetView from './../../components/AssetView';
+import '../../styles/AdminManageProfile.css';
 
 const ManageMgmtProfile = (props) => {
+
+    const [profileInputs,
+        setProfileInputs] = useState({position: '', fullname: '', profileimg: ''})
+
+    const handleInputChange = () => {
+        console.log('change');
+    }
 
     return (
         <WithAdminAuth>
@@ -14,8 +22,39 @@ const ManageMgmtProfile = (props) => {
                 <div className="ap-box">
                     <AssetView/>
                 </div>
-                <div className="ap-box">
+                <div className="ap-box createprofile-box">
                     <h3>Create New Profile</h3>
+                    <div>
+                        <div className="">
+                            <label htmlFor="position">Position</label>
+                            <input type="text" name="position" id="position" onChange={handleInputChange}/>
+                        </div>
+                        <div className="">
+                            <label htmlFor="fullname">Full Name</label>
+                            <input
+                                type="text"
+                                name="fullname"
+                                id="fullname"
+                                onChange={handleInputChange}/>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="">
+                            <label htmlFor="profileimg">Profile Image&nbsp;<small>(Link)</small>
+                            </label>
+                            <input
+                                type="text"
+                                name="profileimg"
+                                id="profileimg"
+                                onChange={handleInputChange}/>
+                        </div>
+
+                        <div className="">
+                            <label htmlFor="profileabout">About</label>
+                            <textarea name="profileabout"></textarea>
+                        </div>
+                    </div>
+
                 </div>
 
                 <div className="ap-box">
