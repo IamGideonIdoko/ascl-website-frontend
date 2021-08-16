f518217848d5a37bd50de8833da51d0364e0a631
Gideon Idoko <iamgideonidoko@gmail.com>
2021-08-17 00:57:51 +0100

    create profile edit ui

--- src/pages/admin/ManageMgmtProfile.js
+++ src/pages/admin/ManageMgmtProfile.js
@@ -3,14 +3,32 @@ import {connect} from 'react-redux';
 import WithAdminAuth from '../../layouts/WithAdminAuth';
 import AssetView from './../../components/AssetView';
 import '../../styles/AdminManageProfile.css';
+import Select from 'react-select';
+import moment from 'moment';
 
 const ManageMgmtProfile = (props) => {
 
     const [profileInputs,
         setProfileInputs] = useState({position: '', fullname: '', profileimg: ''})
-
+    const [selectedProfile, setSelectedProfile] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+    
     const handleInputChange = () => {
         console.log('change');
+    }
+
+    const handleProfileSelectInputChange = option => {
+        setSelectedProfile(option
+            ? option
+            : null);
+    }
+
+    const handleProfileDelete = () => {
+
+    }
+
+    const handleProfileEdit = () => {
+        
     }
 
     return (
@@ -57,8 +75,55 @@ const ManageMgmtProfile = (props) => {
                 </div>
 
                 <div className="ap-box">
-                    <h3>Update or Delete Profile</h3>
-                    <p>Select the profile who want to update or delete.</p>
+                    <h3>Edit or Delete Profile</h3>
+                    <p>Select the profile who want to edit or delete.</p>
+                    {props.isLoaded && <div className="edit-page-container">
+                        <div className="page-select-wrapper">
+                            <Select
+                                className="asset-form-select"
+                                defaultValue={selectedProfile}
+                                value={selectedProfile}
+                                options={props
+                                .mgmtProfiles
+                                .map(({_id, position, name, created_at}) => ({
+                                    value: _id,
+                                    label: `${position} [${name}] (${moment(created_at).format('MMM DD, YYYY')})`
+                                }))}
+                                onChange={handleProfileSelectInputChange}
+                                isClearable={true}
+                                isSearchable={true}
+                                placeholder={`Select a profile...`}
+                                styles={{
+                                menu: (provided, state) => ({backgroundColor: "var(--primary-color-light)", border: "1px solid var(--primary-color"}),
+                                option: (styles, {isSelected}) => {
+                                    return {
+                                        ...styles,
+                                        backgroundColor: isSelected
+                                            ? 'var(--secondary-color) !important'
+                                            : null
+                                    }
+                                }
+                            }}/>
+                        </div>
+                        <div className="edit-page-action-btns">
+                            <button className="page-edit-btn page-btn" onClick={handleProfileEdit}>Edit</button>
+                            <button
+                                className="page-delete-btn page-btn"
+                                disabled={isDeleting}
+                                onClick={handleProfileDelete}>{isDeleting
+                                    ? 'Deleting...'
+                                    : 'Delete'}</button>
+                        </div>
+                        {/* <div className="edit-page-action-btns">
+                            <button
+                                className="page-delete-btn page-btn"
+                                disabled={isDeleting}
+                                onClick={handleProfileDelete}>{isDeleting
+                                    ? 'Deleting...'
+                                    : 'Delete'}</button>
+                        </div> */}
+                    </div>
+}
                 </div>
 
             </div>}
@@ -66,6 +131,6 @@ const ManageMgmtProfile = (props) => {
     )
 }
 
-const mapStateToProps = state => ({user: state.auth.user, isAuthenticated: state.auth.isAuthenticated});
+const mapStateToProps = state => ({user: state.auth.user, isAuthenticated: state.auth.isAuthenticated, mgmtProfiles: state.mgmtProfile.mgmtProfiles, isLoaded: state.mgmtProfile.isLoaded});
 
 export default connect(mapStateToProps, null)(ManageMgmtProfile);
