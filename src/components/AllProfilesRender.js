cdfe60cd549cc120c8f40dbd2a27b32d79bc2c2b
Gideon Idoko <iamgideonidoko@gmail.com>
2021-08-22 09:54:06 +0100

    update management profile page

---
+++ src/components/AllProfilesRender.js
@@ -0,0 +1,43 @@
+import {Link} from 'react-router-dom';
+// import moment from 'moment';
+
+const AllProfilesRender = ({profiles, category}) => {
+
+    if (profiles.length === 0) {
+        return (
+            <b>No profile yet.</b>
+        )
+    } else {
+        return (
+            <div className="profiles-container">
+                {/* <div className="proc-left">
+                    <ul>
+                        {profiles.map(profile => (
+                            <li key={profile._id} className="">
+                                <Link to={`/management-profile/${profile.slug}`}>{profile.position}</Link>
+                            </li>
+                        ))
+}
+                    </ul>
+                </div> */}
+                <ul>
+                    {profiles.map(profile => (
+                        <li key={profile._id} className="profile-box">
+                            <div>
+                                <img src={profile.photo} alt={profile.name}/>
+                            </div>
+                            <div>
+                                <Link to={`/management-profile/${profile.slug}`}>{profile.name}</Link><br/>
+                                <Link to={`/management-profile/${profile.slug}`}>{profile.position}</Link>
+                            </div>
+
+                        </li>
+                    ))
+}
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default AllProfilesRender;
--- src/pages/ManagementProfile.js
+++ src/pages/ManagementProfile.js
@@ -1,19 +1,27 @@
-import MainLayout from './../layouts/MainLayout';
 import WithSidebar from '../layouts/WithSidebar';
+import MainLayout from './../layouts/MainLayout';
+import {connect} from 'react-redux';
+import AllProfilesRender from '../components/AllProfilesRender';
 
+function News(props) {
 
-function ManagementProfile() {
+    const allProfilesPages = props.mgmtProfiles;
 
     return (
         <MainLayout>
-        <WithSidebar>
-            <h1>Management Profile</h1>
-
-        </WithSidebar>
-
-
+            <WithSidebar>
+                <h1>Management Profile</h1>
+                <p>Find profile of top management staff below:</p>
+                {!props.isLoaded
+                    ? <div>Loading</div>
+                    : <div>
+                        <AllProfilesRender profiles={allProfilesPages}/>
+                    </div>}
+            </WithSidebar>
         </MainLayout>
     )
 }
 
-export default ManagementProfile;
+const mapStateToProps = (state, ownProps) => ({mgmtProfiles: state.mgmtProfile.mgmtProfiles, isLoaded: state.page.isLoaded});
+
+export default connect(mapStateToProps, null)(News);
