5d01df3cef6a2a1e30bdb5fcca102b057197b792
Gideon Idoko <iamgideonidoko@gmail.com>
2021-08-22 06:38:21 +0100

    add backup image for images not found

--- src/components/AllGalleriesRender.js
+++ src/components/AllGalleriesRender.js
@@ -1,18 +1,35 @@
 import {SRLWrapper} from 'simple-react-lightbox';
+import NoImage from '../images/image-not-found.png';
 // import moment from 'moment';
 
 const AllGalleriesRender = ({galleries}) => {
 
+    const galleryOptions = {
+        settings: {
+            overlayColor: "rgba(1, 104, 103, 0.5)",
+            autoplaySpeed: 3000,
+            transitionSpeed: 1200,
+          },
+          buttons: {
+              showDownloadButton: false
+          }
+    }
+
+    const handleImgError = e => {
+        e.target.src = NoImage;
+        e.onerror = null
+    }
+
     if (galleries.length === 0) {
-        return (
+        return ( 
             <b>No Gallery.</b>
         )
     } else {
         return (
-            <SRLWrapper>
+            <SRLWrapper options={galleryOptions}>
                 <ul className="gallery-wrapper">{galleries.map((gallery, idx) => (
                         <li key={gallery._id} className="gallery-box">
-                            <img src={gallery.cover_img} alt="Not Available"/>
+                            <img src={gallery.cover_img} alt={gallery.caption || 'Not Available'} onError={handleImgError} />
                         </li>
                     ))
 }
