2ff0628fb90cd3df317342c9adf2b36e0b0004ae
Gideon Idoko <iamgideonidoko@gmail.com>
2021-08-26 01:30:55 +0100

    style faq page

--- src/components/AllFaqsRender.js
+++ src/components/AllFaqsRender.js
@@ -1,6 +1,18 @@
-
+import {useState} from 'react';
 
 const AllFaqsRender = ({faqs}) => {
+    const [activeFaqs, setActiveFaqs] = useState([]);
+
+    const handleFaqActive = function() {
+        const {id} = this;
+        if (activeFaqs.includes(id)) {
+            setActiveFaqs(prev => prev.filter(x => x !== id));
+        } else {
+            setActiveFaqs(prev => [...prev, id]);
+        }
+    }
+
+    console.log("activeFaqs", activeFaqs);
 
     if (faqs.length === 0) {
         return (
@@ -9,15 +21,16 @@ const AllFaqsRender = ({faqs}) => {
     } else {
         return (
             <div className="faqs-container">
-                <ul>
+                <ul className="faqs-list">
                     {faqs.map(faq => (
                         <li key={faq._id} className="faqs-box">
-                            <div>
-                                <p>{faq.question}</p>
+                            <div onClick={handleFaqActive.bind({id: faq._id})}>
+                                <span>{faq.question}</span>
+                                <span><i className={`neu ${activeFaqs.includes(faq._id) ? 'neu-minus-circle' : 'neu-add-circle'}`}></i></span>
                             </div>
-                            <div>
-                                <p>{faq.answer}</p>
-                            </div>
+                            {activeFaqs.includes(faq._id) && <div>
+                                <span>{faq.answer}</span>
+                            </div>}
 
                         </li>
                     ))
--- src/pages/FAQs.js
+++ src/pages/FAQs.js
@@ -2,6 +2,7 @@ import WithSidebar from '../layouts/WithSidebar';
 import MainLayout from './../layouts/MainLayout';
 import {connect} from 'react-redux';
 import AllFaqsRender from '../components/AllFaqsRender';
+import '../styles/Faqs.css';
 
 function Faqs(props) {
 
