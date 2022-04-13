b5a56a3637a09662bc9d9f04e37568cb56efb100
Gideon Idoko <iamgideonidoko@gmail.com>
2022-04-13 06:35:26 +0100

    Edit info on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,36 +14,12 @@ function Contact() {
                     <h1>Contact Us</h1>
                     <h3>Contact Information:</h3>
 
-                    <h3>Engineering Works & Services:-</h3>
-
-                    <p>Email: sulei_umar@yahoo.com; umarsulei@ajaokutasteel.com<br/>
-
-                        Phone: +2348036048476; +2348053566929
-                    </p>
-
-                    <h3>Commerce Department:
-                    </h3>
-                    <p>+234834028856</p>
-
-                    <h3>Public Affairs & Information:
-                    </h3>
-                    <p>+2438065328250<br/>
-                        E-mail: saliuahmad50@gmail.com<br/>
-
-                        OR<br/>
-
-                        +2348036707200<br/>
-
-                        +2348052174874<br/>
-
-                        Email: frankjamgbadi@gmail.com</p>
-
                     <h3>Email Address:</h3>
-                    <p>info@ajaokutasteel.com
+                    <p>info@ajaokutasteel.com.ng
                     </p>
 
                     <h3>Website:</h3>
-                    <p>www.ajaokutasteel.com</p>
+                    <p>www.ajaokutasteel.com.ng</p>
 
                 </div>
             </WithSidebar>
